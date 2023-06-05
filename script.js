f316738b9852debf201d7ec7a3e08537197765f4
Sirvikkaz <121073465+Sirvikkaz@users.noreply.github.com>
2023-06-05 21:20:54 +0100

    Update 1
    Some Update

--- script.js
+++ script.js
@@ -7,73 +7,55 @@ toggle.addEventListener('click', ()=>{
     if(toggle_value.display=='none'){
         console.log(toggle_value.display)
         appear.style.display='block';
-        appear.style.transition = 'transition: 5s ease-in width';
+        appear.style.opacity = '1';
+        toggle.className = "fa-sharp fa-solid fa-xmark";
         console.log(toggle_value.width)
     }else{
         appear.style.display='none';
+        toggle.className = "fa-solid fa-list";
     }
 })
-//for switching Images
-const images = ["monkey_skeleton.png","monkeyy_nft.png","monkeyyy.png"];
-var content = [
 
-
-]
-var apep = document.getElementsByClassName('main_apep');
-/*if(apep[1]){
-    console.log('Hello')
-document.getElementsByClassName('main_apep')[1].innerHTML = '<p>Bored Ape Smoke Club </p>'
-}*/
 const firstColor = 0;
-const lastColor = images.length-1;
 let currentColor = 0;
 const next = document.querySelector('.next');
 var monkey = document.getElementById("mon_skel")
 const previous = document.querySelector('.previous')
-var apep = document.getElementsByClassName('main_apep');
+var mainArr = document.getElementsByClassName('mainArr');
+const lastColor = mainArr.length-1;
+const mainDiv = ['main3-card1', 'main3-card2', 'main3-card3']
 next.addEventListener('click', ()=>{
-    currentColor++;
-    if(currentColor>lastColor){
-        currentColor=firstColor;
-    }
-    monkey.src=images[currentColor];
-    console.log(images[currentColor])
-    console.log(images[1])
-    if((images[currentColor])==(images[1])){
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Bored Ape Smoke Club</p>';
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">2.23 ETH</p>';
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
-    }
-    else if((images[currentColor])==(images[2])){
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Mutant Ape King Queen</p>';
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">6.08 ETH</p>';
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
-    }
-    else{
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Ape Zombie with Lobster </p>'; 
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">5.12 ETH</p>'; 
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
-    }
+currentColor++
+if(currentColor>lastColor){
+currentColor = firstColor;
+}
+console.log('Hello world')
+mainArr.className = mainArr[currentColor];
+if(currentColor==0){
+    mainArr[lastColor].style.display = 'none';
+    return  mainArr[0].style.display = 'block';
+    //main[lastMain].style.display = 'none';
+      console.log(currentMain)
+      //currentMain = 1;
+  }
+  mainArr[currentColor-1].style.display = 'none';
+  console.log(currentColor)
+  mainArr[currentColor].style.display = 'block';
 })
 previous.addEventListener('click', ()=>{
     currentColor--;
-    if(currentColor<firstColor){
-        currentColor=lastColor;
+    if(currentColor==0){
+    mainArr[currentColor+1].style.display = 'none';
+    mainArr[currentColor].style.display = 'block';
     }
-    monkey.src=images[currentColor]
-    if((images[currentColor])==(images[1])){
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Bored Ape Smoke Club</p>';
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">2.23 ETH</p>';
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
+    else if(currentColor==1){
+    mainArr[currentColor+1].style.display = 'none';
+    mainArr[currentColor].style.display = 'block';
+   
     }
-    else if((images[currentColor])==(images[2])){
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Mutant Ape King Queen</p>';
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">6.08 ETH</p>';
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
-    }
-    else{
-        document.getElementById('main3_cardpp').innerHTML= '<p class="main_apep main3-cardp">Ape Zombie with Lobster </p>';
-        document.querySelector('.main3-size').innerHTML = '<p class="main3-size">5.12 ETH</p>';   
-        document.querySelector('."btn btn-btn').innerHTML = '<a href="" class="btn btn-btn">Place a Bid</a>'
-    }
+   else if(currentColor<firstColor){
+    mainArr[currentColor+1].style.display = 'none';
+    mainArr[lastColor].style.display = 'block';
+    currentColor = 2
+   }
 })
